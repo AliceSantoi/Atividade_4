120197fc2af1e97bb84260842a77dd46f76778a9
Alice Pinheiro <106347376+AliceSantoi@users.noreply.github.com>
2025-11-29 15:57:13 -0300

    Add modificações no codigo

--- main.py
+++ main.py
@@ -13,12 +13,13 @@ fig1 = px.line(df, x = 'Date_reported', y = 'Cumulative_cases', color = "Country
 fig1.update_layout(xaxis_title = 'Data', yaxis_title = 'Casos acumulados')
 fig1.show()
 
+st.plotly_chart(fig1, use_container_width = True)
+
 df_br_eua_ind = df.query("Country == 'Brazil' or Country == 'United States of America' or Country == 'India'")
 
 fig2 = px.pie(df_br_eua_ind, values = 'Cumulative_cases', names = "Country", title = "Casos Covid-19: Brasil, EUA e India")
 fig2.show()
 
-st.plotly_chart(fig1, use_container_width = True)
 st.plotly_chart(fig2, use_container_width = True)
 
 
